b15329ca2d3cfcfb3cbb6dce85d6be713534ab87
igks <igks.business@gmail.com>
2020-02-05 15:59:08 +0700

    add configuration model

---
+++ Migrations/20200205085025_ConfigurationModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BusMeal.API.Migrations
+{
+    public partial class ConfigurationModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Configuration",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RowGrid = table.Column<int>(nullable: false),
+                    LockedBusOrder = table.Column<DateTime>(nullable: false),
+                    LockedMealOrder = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Configuration", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Configuration");
+        }
+    }
+}
