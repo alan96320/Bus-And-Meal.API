269be838e31dfb204231045f91603dc301093a3e
Rusmin Ho <rusmin.ho@gmail.com>
2020-02-05 13:37:33 +0700

    Add Model

---
+++ Core/Models/Department.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusMeal.API.Core.Models
+{
+    [Table("Department")]
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+
+        public string Code {get; set;}
+
+        [Required]
+        [MaxLength(255)]
+        public string Name {get; set;}
+
+    }
+}
