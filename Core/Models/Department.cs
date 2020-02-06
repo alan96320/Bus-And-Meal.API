9ece87d708b98415e18a5e5c800f40ea338bd4d3
kade <sugiantara.igk@gmail.com>
2020-02-06 14:37:04 +0700

    revise http put method

--- Core/Models/Department.cs
+++ Core/Models/Department.cs
@@ -3,21 +3,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BusMeal.API.Core.Models
 {
-    [Table("Department")]
-    public class Department
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+  [Table("Department")]
+  public class Department
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+    [Required]
+    [MaxLength(50)]
 
-        public string Code {get; set;}
+    public string Code { get; set; }
 
-        [Required]
-        [MaxLength(255)]
-        public string Name {get; set;}
+    [Required]
+    [MaxLength(255)]
+    public string Name { get; set; }
 
-    }
+  }
 }
