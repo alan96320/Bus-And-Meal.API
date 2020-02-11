658921985bbe36651f74d3e3512e7721c132b20e
kade <sugiantara.igk@gmail.com>
2020-02-11 14:37:58 +0700

    create bus order verification header total model

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -25,6 +25,7 @@ namespace BusMeal.API.Persistence
     public DbSet<BusOrderVerificationHeader> BusOrderVerificationHeader { get; set; }
     public DbSet<BusOrderEntryHeader> BusOrderEntryHeader { get; set; }
     public DbSet<BusOrderEntryDetail> BusOrderEntryDetail { get; set; }
+    public DbSet<BusOrderVerificationHeaderTotal> BusOrderVerificationHeaderTotal { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
