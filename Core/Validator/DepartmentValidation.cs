a5385c9261f513d572f772b8378b75b4095d8bab
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2020-02-20 13:56:58 +0700

    revise custom message department validator

--- Core/Validator/DepartmentValidation.cs
+++ Core/Validator/DepartmentValidation.cs
@@ -8,8 +8,8 @@ namespace BusMeal.API.Core.Validator
   {
     public DepartmentValidation()
     {
-      RuleFor(d => d.Name).Length(1, 255);
-      RuleFor(d => d.Code).Length(1, 255);
+      RuleFor(d => d.Name).Length(1, 100).WithMessage("Panjang karakter tidak boleh kurang dari 1 dan tidak lebih dari 100");
+      RuleFor(d => d.Code).Length(1, 50).WithMessage("Panjang karakter tidak boleh kurang dari 1 dan tidak lebih dari 50");
     }
   }
 }
