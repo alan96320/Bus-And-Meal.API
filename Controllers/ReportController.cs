1a900502555426bf6c208b5dc624250822ebe9b7
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2020-03-06 13:45:32 +0700

    add controller report to serve meal type report request

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -11,19 +11,37 @@ namespace BusMeal.API.Controllers
   public class ReportController : Controller
   {
     private readonly IMapper mapper;
+
+    public IMealtypeRepository mealtypeRepository { get; }
+
     private readonly IDepartmentRepository departmentRepository;
-    public ReportController(IMapper mapper, IDepartmentRepository departmenRepository)
+    public ReportController(
+    IMapper mapper,
+    IDepartmentRepository departmenRepository,
+    IMealtypeRepository mealtypeRepository
+    )
     {
       this.departmentRepository = departmenRepository;
       this.mapper = mapper;
+      this.mealtypeRepository = mealtypeRepository;
     }
 
     [HttpGet("department")]
-    public async Task<IActionResult> GetAll()
+    public async Task<IActionResult> GetDepartmentReport()
     {
       var departments = await departmentRepository.GetAll();
 
       var result = mapper.Map<IEnumerable<ViewDepartmentResource>>(departments);
+
+      return Ok(result);
+    }
+
+    [HttpGet("mealtype")]
+    public async Task<IActionResult> GetMealTypeReport()
+    {
+      var mealtypes = await mealtypeRepository.GetAll();
+
+      var result = mapper.Map<IEnumerable<ViewDepartmentResource>>(mealtypes);
 
       return Ok(result);
     }
