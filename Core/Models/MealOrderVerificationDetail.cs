6628c050c64ee941b5272680fcf6d42f83185742
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2020-03-06 10:40:42 +0700

    meal order on save verification

--- Core/Models/MealOrderVerificationDetail.cs
+++ Core/Models/MealOrderVerificationDetail.cs
@@ -8,14 +8,11 @@ namespace BusMeal.API.Core.Models
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-
     public MealOrderVerification MealOrderVerification { get; set; }
     public int MealOrderVerificationId { get; set; }
     public MealType MealType { get; set; }
     public int MealTypeId { get; set; }
-
     public int? VendorId { get; set; }
-
     public int SumOrderQty { get; set; }
     public int AdjusmentQty { get; set; }
     public int SwipeQty { get; set; }
