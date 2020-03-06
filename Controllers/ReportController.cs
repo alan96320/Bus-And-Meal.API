12cbb956ffa1ce1be9ee2f462c30e9eb4d2ea779
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2020-03-06 14:25:08 +0700

    add controller for employee, dormblock, mealtype report

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -11,19 +11,23 @@ namespace BusMeal.API.Controllers
   public class ReportController : Controller
   {
     private readonly IMapper mapper;
-
     public IMealtypeRepository mealtypeRepository { get; }
-
+    private readonly IEmployeeRepository employeeRepository;
+    private IDormitoryBlockRepository dormitoryBlockRepository;
     private readonly IDepartmentRepository departmentRepository;
     public ReportController(
     IMapper mapper,
     IDepartmentRepository departmenRepository,
-    IMealtypeRepository mealtypeRepository
+    IMealtypeRepository mealtypeRepository,
+    IEmployeeRepository employeeRepository,
+    IDormitoryBlockRepository dormitoryBlockRepository
     )
     {
       this.departmentRepository = departmenRepository;
       this.mapper = mapper;
       this.mealtypeRepository = mealtypeRepository;
+      this.employeeRepository = employeeRepository;
+      this.dormitoryBlockRepository = dormitoryBlockRepository;
     }
 
     [HttpGet("department")]
@@ -41,7 +45,27 @@ namespace BusMeal.API.Controllers
     {
       var mealtypes = await mealtypeRepository.GetAll();
 
-      var result = mapper.Map<IEnumerable<ViewDepartmentResource>>(mealtypes);
+      var result = mapper.Map<IEnumerable<ViewMealTypeResource>>(mealtypes);
+
+      return Ok(result);
+    }
+
+    [HttpGet("employee")]
+    public async Task<IActionResult> GetEmployeeReport()
+    {
+      var employees = await employeeRepository.GetAll();
+
+      var result = mapper.Map<IEnumerable<ViewDepartmentResource>>(employees);
+
+      return Ok(result);
+    }
+
+    [HttpGet("dormitoryblock")]
+    public async Task<IActionResult> GetDormitoryBlockReport()
+    {
+      var dormitoryblocks = await dormitoryBlockRepository.GetAll();
+
+      var result = mapper.Map<IEnumerable<ViewDepartmentResource>>(dormitoryblocks);
 
       return Ok(result);
     }
