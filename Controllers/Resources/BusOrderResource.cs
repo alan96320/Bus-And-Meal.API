b5caa5ecd57c3620f209bc129669c9b238d36081
Rusmin Ho <rusmin.ho@gmail.com>
2020-03-08 19:44:59 +0700

    Add UserId at BusOrder Resource

--- Controllers/Resources/BusOrderResource.cs
+++ Controllers/Resources/BusOrderResource.cs
@@ -15,7 +15,8 @@ namespace BusMeal.API.Controllers.Resources
     public int? BusOrderVerificationId { get; set; }
 
     public bool isReadyToCollect {get;set;}
-
+    public int UserId { get; set; }
+    
     public ICollection<SaveBusOrderDetailResource> BusOrderDetails { get; set; }
       = new Collection<SaveBusOrderDetailResource>();
   }
@@ -26,15 +27,11 @@ namespace BusMeal.API.Controllers.Resources
     public DateTime OrderEntryDate { get; set; }
 
     public int? DepartmentId { get; set; }
-    public ViewDepartmentResource Department {get;set;}
-
     public int? DormitoryBlockId { get; set; }
-    public ViewDormitoryBlockResource DomitoryBlock {get;set;}
-
     public int? BusOrderVerificationHeaderId { get; set; }
 
     public bool IsReadyToCollect {get;set;}
-
+    public int UserId { get; set; }
     public ICollection<ViewBusOrderDetailResource> BusOrderDetails { get; set; }
      = new Collection<ViewBusOrderDetailResource>();
   }
