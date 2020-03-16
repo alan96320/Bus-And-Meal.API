0beb9ceb4123ce2813d9cd8b5289d58fdb387ed5
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2020-03-16 08:13:23 +0700

    remove row grid from configuration

--- Controllers/Resources/ConfigurationResource.cs
+++ Controllers/Resources/ConfigurationResource.cs
@@ -2,7 +2,6 @@ namespace BusMeal.API.Controllers.Resources
 {
   public class SaveConfigurationResource
   {
-    public int RowGrid { get; set; }
     public string LockedBusOrder { get; set; }
     public string LockedMealOrder { get; set; }
   }
@@ -10,7 +9,6 @@ namespace BusMeal.API.Controllers.Resources
   public class ViewConfigurationResource
   {
     public int Id { get; set; }
-    public int RowGrid { get; set; }
     public string LockedBusOrder { get; set; }
     public string LockedMealOrder { get; set; }
   }
