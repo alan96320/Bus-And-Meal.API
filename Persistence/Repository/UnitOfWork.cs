0e37275254e82d34645b11da1fdf895ab335a05e
Rusmin Ho <rusmin.ho@gmail.com>
2020-03-19 14:57:55 +0700

    Update UnitOfWork using TryCatch

--- Persistence/Repository/UnitOfWork.cs
+++ Persistence/Repository/UnitOfWork.cs
@@ -3,18 +3,33 @@ using BusMeal.API.Core.IRepository;
 
 namespace BusMeal.API.Persistence.Repository
 {
-  public class UnitOfWork : IUnitOfWork
-  {
-    private readonly DataContext context;
-
-    public UnitOfWork(DataContext context)
+    public class UnitOfWork : IUnitOfWork
     {
-      this.context = context;
-    }
+        private readonly DataContext context;
 
-    public async Task<bool> CompleteAsync()
-    {
-      return await context.SaveChangesAsync() > 0;
+        public UnitOfWork(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> CompleteAsync()
+        {
+            int saveResult = 0;
+            try
+            {
+                saveResult = await context.SaveChangesAsync();
+            }
+            catch
+            {
+                saveResult = 0; // Fix Me : Make it more spesific with negatif value for :
+                                //  catch (DbUpdateException ex)
+                                //  catch (DbUpdateException ex)
+
+            }
+            return saveResult > 0;
+
+
+
+        }
     }
-  }
 }
