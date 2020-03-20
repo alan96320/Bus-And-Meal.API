ff453028564bf6f9f4756c6f2726d218a71fe6dd
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2020-03-20 14:56:35 +0700

    add vendor id in verification detail resource

--- Controllers/Resources/MealOrderVerificationDetailResource.cs
+++ Controllers/Resources/MealOrderVerificationDetailResource.cs
@@ -19,6 +19,7 @@ namespace BusMeal.API.Controllers.Resources
     public ViewMealTypeResource MealType { get; set; }
     public int SumOrderQty { get; set; }
     public int AdjusmentQty { get; set; }
+    public int VendorId { get; set; }
     public int SwipeQty { get; set; }
     public int LogBookQty { get; set; }
   }
