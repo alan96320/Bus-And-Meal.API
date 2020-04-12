f46a11fd214d89c3e3695c4170aa0ed5c447c29b
Kade <sugiantara.igk@gmail.com>
2020-04-12 14:50:40 +0700

    add data base error in start up

--- Startup.cs
+++ Startup.cs
@@ -143,27 +143,29 @@ namespace BusMeal.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
             }
             else
             {
                 app.UseExceptionHandler(builder =>
                 {
                     builder.Run(async context =>
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                var error = context.Features.Get<IExceptionHandlerFeature>();
-                if (error != null)
-                {
-                    context.Response.AddApplicationError(error.Error.Message);
-                    await context.Response.WriteAsync(error.Error.Message);
-                }
-            });
+                            var error = context.Features.Get<IExceptionHandlerFeature>();
+                            if (error != null)
+                            {
+                                context.Response.AddApplicationError(error.Error.Message);
+                                await context.Response.WriteAsync(error.Error.Message);
+                            }
+                        });
                 });
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 // app.UseHsts();
-            }
 
+            }
+            // app.UseDatabaseErrorPage();
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
