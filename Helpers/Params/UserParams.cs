d719b68fadda3bcb561dcc36a3a2c29c60e67f8a
Kade <sugiantara.igk@gmail.com>
2020-04-14 00:39:27 +0700

    add filter for user

--- Helpers/Params/UserParams.cs
+++ Helpers/Params/UserParams.cs
@@ -1,9 +1,11 @@
 namespace BusMeal.API.Helpers.Params
 {
-  public class UserParams : BaseParams
-  {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public bool isActive { get; set; } = true;
-  }
+    public class UserParams : BaseParams
+    {
+        public string GddbId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public bool isActive { get; set; } = true;
+    }
 }
